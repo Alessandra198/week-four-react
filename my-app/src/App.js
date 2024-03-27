549ab7d508642094e208ce5d109c89621f1a21e7
Alessandra198 <rinaldoalessandra@gmail.com>
2024-03-28 00:41:19 +0100

    Trying to make netlify work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
+
 import Search from "./Search";
 import City from "./City";
 import Forecast from "./Forecast";
 import Footer from "./Footer";
 
-export default function Apps() {
+export default function App() {
   return (
-    <div className="Apps">
+    <div className="App">
       <Search />
       <City />
       <Forecast />
